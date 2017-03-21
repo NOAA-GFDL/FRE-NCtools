b6acb3ce183c8f3a74a556cafc19ab933b4dfc89
Seth Underwood <Seth.Underwood@noaa.gov>
2017-03-21 13:04:43 -0400

    Correct theia build.mk file
    The theia build.mk file had an extra space character.  That extra character has been removed.
    
    #12

--- site-configs/theia/build.mk
+++ site-configs/theia/build.mk
@@ -10,7 +10,7 @@ FLIBS_SITE :=
 
 LDFLAGS='-Wl,-rpath=/apps/intel/composer_xe_2015.1.133/compiler/lib/intel64,-rpath=/apps/hdf5/1.8.14-intel/lib,-rpath=/apps/netcdf/4.3.0-intel/lib'
 
-NETCDF_HOME := /apps/netcdf/4.3.0-intel 
+NETCDF_HOME := /apps/netcdf/4.3.0-intel
 HDF5_HOME := /apps/hdf5/1.8.14-intel
 
 STATIC :=
