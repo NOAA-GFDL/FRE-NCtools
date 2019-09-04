ead480d4db330cc06b5a544815572adcd913ad1b
Seth Underwood <Seth.Underwood@noaa.gov>
2019-09-04 10:17:09 -0400

    Update gfdl site configs
    Removed gfdl/build.mk, added gfdl/config.site, slight modifications to gfdl/env.sh

--- site-configs/gfdl/env.sh
+++ site-configs/gfdl/env.sh
@@ -4,16 +4,14 @@
 source /usr/local/Modules/default/init/sh
 module load intel_compilers/15.0.1 mpich2/1.2.1p1 netcdf/4.2
 
+# Need a newer autoconf/automake than what is curerntly on the system.
+# This is needed until there is a module for these.
+PATH=/app/spack/linux-rhel6-x86_64/gcc-4.4.7/autoconf/2.69-oz56rghtag37aapzrgmh2tzhyjek7o6c/bin:/app/spack/linux-rhel6-x86_64/gcc-4.4.7/automake/1.16.1-shm646foq3qrojtsqhc6uat77auqexx7/bin:${PATH}
+export PATH
+
 # **********************************************************************
 # Set environment variablesSetup and Load the Modules
 # **********************************************************************    
 LD_RUN_PATH=$LD_LIBRARY_PATH
 export LD_RUN_PATH
 
-# **********************************************************************
-# Aliases
-# **********************************************************************    
-
-# **********************************************************************
-# Other build configuration settings
-# **********************************************************************    
