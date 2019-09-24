5504cec9a60354654fc0b46c4368b385b7e63546
Uriel Ramirez <uriel.ramirez@noaa.gov>
2019-09-24 13:01:25 -0400

    Gets the test_grid_coupled_model test to pass?

--- t/test_grid_coupled_model.sh
+++ t/test_grid_coupled_model.sh
@@ -4,6 +4,16 @@
 
 @test "Test grid for coupled model (land and atmosphere are C48 and ocean is 1 degree tripolar grid)" {
 
+mkdir work_dir
+cd work_dir
+cp $top_srcdir/t/grid_coupled_model/OCCAM_p5degree.nc OCCAM_p5degree.nc
+cp $top_srcdir/t/grid_coupled_model/C48_grid.tile1.nc C48_grid.tile1.nc
+cp $top_srcdir/t/grid_coupled_model/C48_grid.tile2.nc C48_grid.tile2.nc
+cp $top_srcdir/t/grid_coupled_model/C48_grid.tile3.nc C48_grid.tile3.nc
+cp $top_srcdir/t/grid_coupled_model/C48_grid.tile4.nc C48_grid.tile4.nc
+cp $top_srcdir/t/grid_coupled_model/C48_grid.tile5.nc C48_grid.tile5.nc
+cp $top_srcdir/t/grid_coupled_model/C48_grid.tile6.nc C48_grid.tile6.nc
+
 #Make_hgrid: create ocean_hgrid"
   run command make_hgrid \
 		--grid_type tripolar_grid \
@@ -34,4 +44,68 @@
 		--tile_file ocean_hgrid.nc \
 		--periodx 360
   [ "$status" -eq 0 ]
+
+#Make_topog: create ocean topography data
+  run command make_topog \
+		--mosaic ocean_mosaic.nc \
+		--topog_type realistic \
+		--topog_file OCCAM_p5degree.nc \
+		--topog_field TOPO \
+		--scale_factor -1 \
+		--vgrid ocean_vgrid.nc \
+		--output topog.nc 
+  [ "$status" -eq 0 ]
+
+#Skipping this for now because it fails 
+#  run command mpirun -n 2 make_topog_parallel \
+#		--mosaic ocean_mosaic.nc \
+#		--topog_type realistic \
+#		--topog_file OCCAM_p5degree.nc \
+#		--topog_field TOPO \
+#		--scale_factor -1 \
+#		--vgrid ocean_vgrid.nc \
+#		--output topog_parallel.nc
+#  [ "$status" -eq 0 ]
+
+#  run command nccmp -md topog.nc topog_parallel.nc
+#  [ "$status" -eq 0 ]
+
+#Make_hgrid: create C48 grid for atmos/land
+  run command make_hgrid \
+		--grid_type gnomonic_ed \
+		--nlon 96 \
+		--grid_name C48_grid \
+  [ "$status" -eq 0 ]
+
+#Make_solo_mosaic: create C48 solo mosaic for atmos/land
+  run command make_solo_mosaic \
+		--num_tiles 6 \
+		--dir . \
+		--mosaic C48_mosaic \
+		--tile_file C48_grid.tile1.nc,C48_grid.tile2.nc,C48_grid.tile3.nc,C48_grid.tile4.nc,C48_grid.tile5.nc,C48_grid.tile6.nc
+  [ "$status" -eq 0 ]
+
+#Make_coupler_mosaic: coupler_mosaic with and without parallel and compare
+  run command make_coupler_mosaic \
+		--atmos_mosaic C48_mosaic.nc \
+		--ocean_mosaic ocean_mosaic.nc \
+		--ocean_topog  topog.nc \
+		--check \
+		--area_ratio_thresh 1.e-10 \
+  [ "$status" -eq 0 ]
+
+#Skipping this for now because it fails 
+#  run command cd parallel
+
+#  run command aprun -n 2 make_coupler_mosaic_parallel \
+#		--atmos_mosaic ../C48_mosaic.nc \
+#		--ocean_mosaic ../ocean_mosaic.nc \
+#		--ocean_topog  ../topog.nc \
+#		--mosaic_name grid_spec  \
+#		--area_ratio_thresh 1.e-10
+#  [ "$status" -eq 0 ]
+
+#Remove the workdir 
+cd ..
+rm -rf work_dir
 }
