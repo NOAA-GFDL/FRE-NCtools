18d8b43f3ee0e31bdeb213f36fbbd7095947dab1
Uriel Ramirez <uriel.ramirez@noaa.gov>
2019-09-25 12:03:15 -0400

    Added test_check_mask.sh for baltic1 experiment

---
+++ t/test_check_mask.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bats
+# Test check_mask: create mask_table to mask out some all-land domain
+# to save processor usage for a sea-ice model, baltic1 experiment
+
+@test "Test check_ mask for baltic1 experiment" {
+
+mkdir work_dir_7
+cd work_dir_7
+cp $top_srcdir/t/checkmask_baltic1/* .
+
+  run command check_mask \
+		--grid_file baltic1_grid_spec.nc \
+		--min_pe 60 \
+		--max_pe 80 
+  [ "$status" -eq 0 ]
+
+cd ..
+rm -rf work_dir_7
