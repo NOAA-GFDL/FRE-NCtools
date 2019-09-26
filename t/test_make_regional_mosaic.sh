1b303c8cb7baa5a815e753adafe67acf72cc75ca
Uriel Ramirez <uriel.ramirez@noaa.gov>
2019-09-26 10:47:44 -0400

    Added the make_regional_mosaic test, but skipped it for now because the input files it needs are too big

---
+++ t/test_make_regional_mosaic.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bats
+# Test make_regional_mosaic: create mosaic file for regional output and do remapping
+#             for regional output
+
+@test "Test make_regional_mosaic" {
+  skip "Skip for now because the input files are too big" 
+  mkdir work_dir_10
+  cd work_dir_10
+  cp $top_srcdir/t/regional_regrid/* . 
+
+  run command make_regional_mosaic \
+		--global_mosaic C192-rot-120.-0.-r1_mosaic.nc \
+		--regional_file rregionatmos_4xdaily_eq_avg.tile2.nc
+  [ "$status" -eq 0 ] 
+
+  run command make_regional_mosaic \
+		--global_mosaic C192-rot-120.-0.-r1_mosaic.nc \
+		--regional_file rregionatmos_4xdaily_eq_avg.tile3.nc
+  [ "$status" -eq 0 ] 
+
+  run command make_regional_mosaic  \
+		--global_mosaic C192-rot-120.-0.-r1_mosaic.nc  \
+		--regional_file rregionatmos_4xdaily_eq_avg.tile5.nc
+  [ "$status" -eq 0 ] 
+
+  run command make_regional_mosaic  \
+		--global_mosaic C192-rot-120.-0.-r1_mosaic.nc  \
+		--regional_file rregionatmos_4xdaily_eq_avg.tile6.nc
+  [ "$status" -eq 0 ] 
+
+#Create regional mosaic
+  run command make_solo_mosaic  \
+		--num_tiles 4  \
+		--dir ./  \
+		--mosaic regional_mosaic  \
+		--tile_file regional_grid.tile2.nc,regional_grid.tile3.nc,regional_grid.tile5.nc,regional_grid.tile6.nc
+  [ "$status" -eq 0 ] 
+
+#Create regular lat-lon grid (100:160, -15:15, size is 360x180)
+  run command make_hgrid  \
+		--grid_type regular_lonlat_grid  \
+		--nxbnd 2  \
+		--nybnd 2  \
+		--xbnd 0,360  \
+		--ybnd -2,2  \
+		--nlon 720  \
+		--nlat 10  \
+		--grid_name latlon_grid
+  [ "$status" -eq 0 ] 
+
+#Create lat-lon mosaic
+  run command make_solo_mosaic  \
+		--num_tiles 1  \
+		--dir ./  \
+		--mosaic_name latlon_mosaic  \
+		--tile_file latlon_grid.nc
+  [ "$status" -eq 0 ] 
+
+#Remap data
+  run command fregrid  \
+		--input_mosaic regional_mosaic.nc  \
+		--output_mosaic latlon_mosaic.nc  \
+		--input_file rregionatmos_4xdaily_eq_avg  \
+		--output_file atmos_4xdaily_latlon.nc  \
+		--scalar_field ps  \
+		--check_conserve
+  [ "$status" -eq 0 ] 
+
+  cd ..
+  rm -rf work_dir_10
+
+}
+
+
