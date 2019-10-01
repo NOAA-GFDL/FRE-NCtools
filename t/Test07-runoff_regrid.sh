5b778f542aca41582a2f76cef15946199cf98d5e
Uriel Ramirez <uriel.ramirez@noaa.gov>
2019-10-01 12:14:42 -0400

    Clean up the Test07-input

--- t/Test07-runoff_regrid.sh
+++ t/Test07-runoff_regrid.sh
@@ -9,6 +9,16 @@
   fi
 
   cd Test07
+  cp $top_srcdir/t/Test06-input/ocean_hgrid.nc ocean_hgrid.nc
+  cp $top_srcdir/t/Test06-input/ocean_mosaic.nc ocean_mosaic.nc
+  cp $top_srcdir/t/Test06-input/ocean_vgrid.nc ocean_vgrid.nc
+  cp $top_srcdir/t/Test03-input/C48_grid.tile1.nc C48_grid.tile1.nc
+  cp $top_srcdir/t/Test03-input/C48_grid.tile2.nc C48_grid.tile2.nc
+  cp $top_srcdir/t/Test03-input/C48_grid.tile3.nc C48_grid.tile3.nc
+  cp $top_srcdir/t/Test03-input/C48_grid.tile4.nc C48_grid.tile4.nc
+  cp $top_srcdir/t/Test03-input/C48_grid.tile5.nc C48_grid.tile5.nc
+  cp $top_srcdir/t/Test03-input/C48_grid.tile6.nc C48_grid.tile6.nc
+  cp $top_srcdir/t/Test03-input/C48_mosaic.nc C48_mosaic.nc
   cp $top_srcdir/t/Test07-input/* .
 
   run command runoff_regrid \
