20cf2cad18d2f6056d85aa137ff7f97e5531ecbf
Chris Blanton <cblanton@Orion-login-4.HPC.MsState.Edu>
2020-02-17 15:17:38 -0600

    #55 comment out desired LD_RUN_PATH environment variable This seems to create static executables that don't require shared libraries, which we want, but it doesn't work on orion as is.

--- site-configs/msu/env.sh
+++ site-configs/msu/env.sh
@@ -9,5 +9,9 @@ module load hdf5
 # **********************************************************************
 # Set environment variablesSetup and Load the Modules
 # **********************************************************************
-LD_RUN_PATH=$LD_LIBRARY_PATH
-export LD_RUN_PATH
+# Setting LD_RUN_PATH seems to make static executables that don't require 
+# libraries to run. This is generally desired but doesn't work on orion so far.
+# FRE loads intel/2019.5 (in order to load nco, which is in the lmod hierarchy)
+# so the nctools works, but this is unoptimal
+#LD_RUN_PATH=$LD_LIBRARY_PATH
+#export LD_RUN_PATH
