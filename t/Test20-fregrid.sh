32795ba21c502c491471227632c093fab38f6f9f
Luis Sal-bey <Luis.Sal-bey@ldt-1467429.gfdl.noaa.gov>
2020-03-17 13:55:12 -0400

    #28 test fregrid script adjusted

--- t/Test20-fregrid.sh
+++ t/Test20-fregrid.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bats
 # test regrid ocean restart file 
 
-@test "Test remap ocean restart file" {
-  skip "TO DO: the input files needed are too large" 
+@test "Test fregrid ocean data" {
 
   if [ ! -d "Test20" ] 
   then
@@ -11,7 +10,28 @@
 
   cd Test20
   cp /archive/fms/tools/input/CM2.1_to_CM2.5/CM2.1_mosaic.nc .
+  cp /archive/fms/tools/input/CM2.1_to_CM2.5/CM2.1_grid.nc .
   cp /archive/fms/tools/input/CM2.1_to_CM2.5/ocean_temp_salt.res.nc . 
+
+#Create regular lat-lon grid (100:160, -15:15, size is 360x180)
+  run command make_hgrid  \
+                --grid_type regular_lonlat_grid  \
+                --nxbnd 2  \
+                --nybnd 2  \
+                --xbnd 0,360  \
+                --ybnd -2,2  \
+                --nlon 720  \
+                --nlat 10  \
+                --grid_name latlon_grid
+  [ "$status" -eq 0 ]
+
+#Create lat-lon mosaic
+  run command make_solo_mosaic  \
+                --num_tiles 1  \
+                --dir ./  \
+                --mosaic_name latlon_mosaic  \
+                --tile_file latlon_grid.nc
+  [ "$status" -eq 0 ]
 
 #Remap data from CM2.1 ocean grid onto regular lat-lon grid.
   run command fregrid   \
