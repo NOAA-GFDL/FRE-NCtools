75e34fac32e437f5bf6ec76c71451304c1b549d1
Luis Sal-bey <Luis.Sal-bey@ldt-1467429.gfdl.noaa.gov>
2020-04-17 10:13:43 -0400

    #57 replaced routine read_mosaic.c and replace directory FMS_HOME for deploy ncrc in gitlab-ci.yml file

--- tools/libfrencutils/read_mosaic.h
+++ tools/libfrencutils/read_mosaic.h
@@ -21,6 +21,6 @@ void read_mosaic_grid_sizes(const char *mosaic_file, int *nx, int *ny);
 void read_mosaic_contact(const char *mosaic_file, int *tile1, int *tile2, int *istart1, int *iend1,
 			 int *jstart1, int *jend1, int *istart2, int *iend2, int *jstart2, int *jend2);
 void read_mosaic_grid_data(const char *mosaic_file, const char *name, int nx, int ny,
-                           double *data, unsigned int level, int ioff, int joff);
+                           double *data, int level, int ioff, int joff);
 
 #endif
