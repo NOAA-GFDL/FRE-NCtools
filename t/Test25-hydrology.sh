8fa66436c178e6847892528b7a392e8b4556f5ac
Chris Blanton <Chris.Blanton@ldt-4359643.gfdl.noaa.gov>
2020-06-19 16:11:50 -0400

    #57 try to fix hydrology tests

--- t/Test25-hydrology.sh
+++ t/Test25-hydrology.sh
@@ -9,11 +9,8 @@
 
   cd Test25
 
-  cp $top_srcdir/tools/simple_hydrog/network_river_fill_coast.nc .
-  cp $top_srcdir/tools/simple_hydrog/gigbp2_0ll.nc .
-
   #Run wrapper hydrology script
-  $top_srcdir/tools/simple_hydrog/run.cr_hydrology.csh -f 0. -t 1.e-5 -m $top_srcdir/t/Test25-input/grid_spec.nc
+  $top_srcdir/tools/simple_hydrog/make_simple_hydrog.csh -f 0. -t 1.e-5 -m $top_srcdir/t/Test25-input/grid_spec.nc
   status=$(echo "$?")
   [ "$status" -eq 0 ]
   [ -e lake_frac.tile6.nc ]
