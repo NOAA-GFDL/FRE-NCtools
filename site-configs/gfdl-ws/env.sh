7dc2f6ac72d3124e71960d9267d5879a45168fcc
Niki Zadeh <Niki.Zadeh@noaa.gov>
2021-02-11 14:43:21 -0500

    Fix gfdl-ws environments

--- site-configs/gfdl-ws/env.sh
+++ site-configs/gfdl-ws/env.sh
@@ -3,10 +3,12 @@
 # **********************************************************************
 source /usr/local/Modules/default/init/sh
 module use -a /home/fms/local/modulefiles
-module load netcdf/4.2
 # netcdf/4.2 loads intel-11, so unload it now
+module unload netcdf/4.2
+module unload hdf5/1.8.8
 module unload intel_compilers/11.1.073
 module load intel_compilers/18.0.5
+module load netcdf/4.2
 module load nccmp
 module use /home/sdu/publicmodules
 module load mpich2/1.5b1
