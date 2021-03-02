d0c4d61dfbff9b47514624afcae57ddb978a1df7
Chris Blanton <Chris.Blanton@ldt-4359643.gfdl.noaa.gov>
2021-03-02 10:14:11 -0500

    #37 working gfdl-ws env.sh using non-spack intel and gcc

--- site-configs/gfdl-ws/env.sh
+++ site-configs/gfdl-ws/env.sh
@@ -2,15 +2,25 @@
 # Setup and Load the Modules
 # **********************************************************************
 source /usr/local/Modules/default/init/sh
-module use /app/spack/default/modulefiles
-module load intel_compilers/18.0.5
+
+module load intel_compilers/19.0.5
+
+# gcc is needed for icc to use newer C11 constructs
+# we want the spack-installed gcc/9.2.0 but configure fails:
+### checking whether /app/spack/v0.15/linux-rhel7-x86_64/gcc-4.8.5/gcc/9.2.0-jlfyxwjebljjliixtaduabmcnh7zeg3l/bin/gfortran understands -c and -o together... no
+### checking for Fortran flag to compile .f90 files... unknown
+### configure: error: Fortran could not compile .f90 files
+#module load gcc/9.2.0
+module load gcc/6.2.0
+
+# nccmp and bats needed only for testing
+module load nccmp bats
+
+# use intel-compiled libraries
+module use /app/spack/v0.15/modulefiles-intel-19.0.5/linux-rhel7-x86_64
 module load netcdf-c/4.7.3
 module load netcdf-fortran/4.5.2
 module load mpich/3.3.2
-# gcc is needed for icc to use newer C11 constructs
-module load gcc/9.2.0
-# nccmp and bats needed only for testing
-module load nccmp bats
 
 # **********************************************************************
 # Set environment variablesSetup and Load the Modules
