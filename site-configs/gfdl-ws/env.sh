80031c34d229cef3ef40c230180576367de34412
Seth Underwood <Seth.Underwood@noaa.gov>
2021-03-04 12:33:39 -0500

    Update gfdl-ws/env.sh to use spack v0.15 module
    Update the gfdl-ws env.sh file to use the v0.15 spack modulefiles

--- site-configs/gfdl-ws/env.sh
+++ site-configs/gfdl-ws/env.sh
@@ -1,22 +1,30 @@
 # **********************************************************************
 # Setup and Load the Modules
 # **********************************************************************
-source /usr/local/Modules/default/init/sh
-module use -a /home/fms/local/modulefiles
-module load netcdf/4.2
-# netcdf/4.2 loads intel-11, so unload it now
-module unload intel_compilers/11.1.073
-module load intel_compilers/18.0.5
-module load nccmp
-module use /home/sdu/publicmodules
-module load mpich2/1.5b1
+ENV_VERSION=v0.15
+INTEL_VERSION=18.0.5
 
-# Add bats to PATH
-PATH=${PATH}:/home/sdu/opt/bats/bin
-export PATH
+source /usr/local/Modules/default/init/sh
+module use /app/spack/${ENV_VERSION}/modulefiles/linux-rhel7-x86_64
+# GCC is needed for icc to use newer C11 constructs
+module load gcc/9.2.0
+# bats is needed for tests
+module load bats/0.4.0
+
+module load intel_compilers/${INTEL_VERSION}
+module use /app/spack/${ENV_VERSION}/modulefiles-intel-${INTEL_VERSION}/linux-rhel7-x86_64
+module load netcdf-c/4.7.3
+module load netcdf-fortran/4.5.2
+module load mpich/3.3.2
 
 # **********************************************************************
 # Set environment variablesSetup and Load the Modules
 # **********************************************************************
-LD_RUN_PATH=$LD_LIBRARY_PATH
-export LD_RUN_PATH
+# The GCC module sets CC and FC to gcc.  Since we want to use icc/ifort
+# we will change CC and FC (and a few others)
+FC=ifort
+CC=icc
+F77=ifort
+CXX=icpc
+export FC CC F77 CXX
+
