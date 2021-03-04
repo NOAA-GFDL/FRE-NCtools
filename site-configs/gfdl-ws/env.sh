511415ffa2faafed5a4c69bb8f4f67a0470448b1
Chris Blanton <Chris.Blanton@ldt-4359643.gfdl.noaa.gov>
2021-03-04 17:17:01 -0500

    #37 keep intel and spack versions as env variable settings (stolen from Seth)

--- site-configs/gfdl-ws/env.sh
+++ site-configs/gfdl-ws/env.sh
@@ -1,9 +1,12 @@
 # **********************************************************************
 # Setup and Load the Modules
 # **********************************************************************
+ENV_VERSION=v0.15
+INTEL_VERSION=19.0.5
+
 source /usr/local/Modules/default/init/sh
 
-module load intel_compilers/19.0.5
+module load intel_compilers/${INTEL_VERSION}
 
 # gcc is needed for icc to use newer C11 constructs
 module load gcc/9.2.0
@@ -12,13 +15,13 @@ module load gcc/9.2.0
 module load nccmp bats
 
 # use intel-compiled libraries
-module use /app/spack/v0.15/modulefiles-intel-19.0.5/linux-rhel7-x86_64
+module use /app/spack/${ENV_VERSION}/modulefiles-intel-${INTEL_VERSION}/linux-rhel7-x86_64
 module load netcdf-c/4.7.3
 module load netcdf-fortran/4.5.2
 module load mpich/3.3.2
 
 # **********************************************************************
-# Set environment variablesSetup and Load the Modules
+# Set environment variables
 # **********************************************************************
 # this simplifies the setup but only works if you source this from the repo root dir
 export CONFIG_SITE=`pwd`/site-configs/gfdl-ws/config.site
--- site-configs/gfdl/env.sh
+++ site-configs/gfdl/env.sh
@@ -1,33 +1,30 @@
 # **********************************************************************
 # Setup and Load the Modules
 # **********************************************************************
+ENV_VERSION=v0.15
+INTEL_VERSION=19.0.5
+
 source /usr/local/Modules/default/init/sh
 
-module load intel_compilers/19.0.5
+module load intel_compilers/${INTEL_VERSION}
 
-# gcc is needed for icc to use newer C11 constructs
-# we want the spack-installed gcc/4.8.5 but configure fails
-### checking whether /app/spack/v0.15/linux-rhel6-x86_64/gcc-4.4.7/gcc/4.8.5-uz2ynmmyxq4tsrf2tuyewx7dow4iszsx/bin/gfortran accepts -g... yes
-### checking whether /app/spack/v0.15/linux-rhel6-x86_64/gcc-4.4.7/gcc/4.8.5-uz2ynmmyxq4tsrf2tuyewx7dow4iszsx/bin/gfortran understands -c and -o together... no
-### checking for Fortran flag to compile .f90 files... unknown
-### configure: error: Fortran could not compile .f90 files
-#module load gcc/4.8.5
+# gcc>=4.9 is needed for icc to use newer C11 constructs
 module load gcc/5.3.0
 
 # Need a newer autoconf/automake than what is curerntly on the system.
 module load autoconf automake
 
-# only needed for testing
+# needed for testing
 module load nccmp bats
 
 # use intel-compiled libraries
-module use /app/spack/v0.15/modulefiles-intel-19.0.5/linux-rhel6-x86_64
+module use /app/spack/${ENV_VERSION}/modulefiles-intel-${INTEL_VERSION}/linux-rhel6-x86_64
 module load netcdf-c/4.7.3
 module load netcdf-fortran/4.5.2
 module load mpich/3.3.2
 
 # **********************************************************************
-# Set environment variablesSetup and Load the Modules
+# Set environment variables
 # **********************************************************************
 # this simplifies the setup but only works if you source this from the repo root dir
 export CONFIG_SITE=`pwd`/site-configs/gfdl/config.site
