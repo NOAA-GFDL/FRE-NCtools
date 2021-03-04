a5769ec5523926e9b21a674bdf90127254a8a3b2
Chris Blanton <Chris.Blanton@ldt-4359643.gfdl.noaa.gov>
2021-03-04 16:54:08 -0500

    #37 use spack-installed gcc for gfdl-ws env.sh

--- site-configs/gfdl-ws/env.sh
+++ site-configs/gfdl-ws/env.sh
@@ -6,12 +6,7 @@ source /usr/local/Modules/default/init/sh
 module load intel_compilers/19.0.5
 
 # gcc is needed for icc to use newer C11 constructs
-# we want the spack-installed gcc/9.2.0 but configure fails:
-### checking whether /app/spack/v0.15/linux-rhel7-x86_64/gcc-4.8.5/gcc/9.2.0-jlfyxwjebljjliixtaduabmcnh7zeg3l/bin/gfortran understands -c and -o together... no
-### checking for Fortran flag to compile .f90 files... unknown
-### configure: error: Fortran could not compile .f90 files
-#module load gcc/9.2.0
-module load gcc/6.2.0
+module load gcc/9.2.0
 
 # nccmp and bats needed only for testing
 module load nccmp bats
