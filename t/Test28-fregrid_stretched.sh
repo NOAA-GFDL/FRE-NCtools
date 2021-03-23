ee880cd002f6914390bcefa7aefec889fc5fc227
Luis Sal-Bey <Luis.Sal-bey@noaa.gov>
2021-03-23 12:52:15 -0400

    # stretched tests updated

---
+++ t/Test28-fregrid_stretched.sh
@@ -0,0 +1,119 @@
+#!/usr/bin/env bats
+# test stretched grid 
+
+@test "Test stretched grid data lats 32.0 34.0 35.4" {
+
+  if [ ! -d "Test28" ] 
+  then
+  		mkdir Test28
+  fi
+
+  cd Test28
+  cp $top_srcdir/t/Test28-input/ocean_hgrid.nc . 
+  cp $top_srcdir/t/Test28-input/ocean_mosaic.nc .
+  cp $top_srcdir/t/Test28-input/topog.nc .
+  cp $top_srcdir/t/Test28-input/fv_tracer.res.tile*.nc .
+
+#Make streetched grid 
+  run command make_hgrid \
+               --grid_type gnomonic_ed --do_schmidt \
+               --stretch_factor 2.5 \
+               --target_lon 262.4 \
+               --target_lat 32.0 \
+               --nlon 512 \
+               --grid_name C384_grid_32.0
+  [ "$status" -eq 0 ]
+
+  run command make_hgrid \
+               --grid_type gnomonic_ed --do_schmidt \
+               --stretch_factor 2.5 \
+               --target_lon 262.4 \
+               --target_lat 32.0 \
+               --nlon 512 \
+               --grid_name C384_grid_34.0
+  [ "$status" -eq 0 ]
+
+  run command make_hgrid \
+               --grid_type gnomonic_ed --do_schmidt \
+               --stretch_factor 2.5 \
+               --target_lon 262.4 \
+               --target_lat 35.4 \
+               --nlon 512 \
+               --grid_name C384_grid_35.4
+  [ "$status" -eq 0 ]
+
+
+#Create stretched grid mosaic
+  run command make_solo_mosaic \
+                --num_tiles 6 \
+                --dir ./ \
+                --mosaic_name C384_mosaic_32.0 --tile_file C384_grid_32.0.tile1.nc,C384_grid_32.0.tile2.nc,C384_grid_32.0.tile3.nc,C384_grid_32.0.tile4.nc,C384_grid_32.0.tile5.nc,C384_grid_32.0.tile6.nc
+  [ "$status" -eq 0 ]
+
+  run command make_solo_mosaic \
+                --num_tiles 6 \
+                --dir ./ \
+                --mosaic_name C384_mosaic_34.0 --tile_file C384_grid_34.0.tile1.nc,C384_grid_34.0.tile2.nc,C384_grid_34.0.tile3.nc,C384_grid_34.0.tile4.nc,C384_grid_34.0.tile5.nc,C384_grid_34.0.tile6.nc
+  [ "$status" -eq 0 ]
+
+  run command make_solo_mosaic \
+                --num_tiles 6 \
+                --dir ./ \
+                --mosaic_name C384_mosaic_35.4 --tile_file C384_grid_35.4.tile1.nc,C384_grid_35.4.tile2.nc,C384_grid_35.4.tile3.nc,C384_grid_35.4.tile4.nc,C384_grid_35.4.tile5.nc,C384_grid_35.4.tile6.nc
+  [ "$status" -eq 0 ]
+
+# stretched grid lats 32.0 34.0 35.4
+  result_32.0="$(mpirun -np 16 fregrid \
+                --input_mosaic C384_mosaic_32.0.nc \
+                --input_file fv_tracer.res \
+                --scalar_field o3 \
+                --nlon 640 \
+                --nlat 400 \
+                --latBegin 15.0 \
+                --latEnd 65.0 \
+                --lonBegin 230.0 \
+                --lonEnd 310.0 \
+                --remap_file fregrid_remap_file_640_by_400_32.0.nc \
+                --output_file out_32.0.nc \
+                --check_conserve)"
+  declare -f conserve_ratio_32.0 = echo $result_32.0 | head -n 5 | rev | cut -c39-49 | rev
+
+  [[ ${conserve_ratio_32.0} < 1. ]]
+
+  result_34.0="$(mpirun -np 16 fregrid \
+                --input_mosaic C384_mosaic_34.0.nc \
+                --input_file fv_tracer.res \
+                --scalar_field o3 \
+                --nlon 640 \
+                --nlat 400 \
+                --latBegin 15.0 \
+                --latEnd 65.0 \
+                --lonBegin 230.0 \
+                --lonEnd 310.0 \
+                --remap_file fregrid_remap_file_640_by_400_34.0.nc \
+                --output_file out_34.0.nc \
+                --check_conserve)"
+  declare -f conserve_ratio_34.0 = echo $result_34.0 | head -n 5 | rev | cut -c39-49 | rev
+
+  [[ ${conserve_ratio_34.0} < 1. ]]
+
+  result_35.4="$(mpirun -np 16 fregrid \
+                --input_mosaic C384_mosaic_35.4.nc \
+                --input_file fv_tracer.res \
+                --scalar_field o3 \
+                --nlon 640 \
+                --nlat 400 \
+                --latBegin 15.0 \
+                --latEnd 65.0 \
+                --lonBegin 230.0 \
+                --lonEnd 310.0 \
+                --remap_file fregrid_remap_file_640_by_400_35.4.nc \
+                --output_file out_35.4.nc \
+                --check_conserve)"
+  declare -f conserve_ratio_35.4 = echo $result_35.4 | head -n 5 | rev | cut -c39-49 | rev
+ 
+
+  [[ ${conserve_ratio_35.4} < 1. ]]
+  cd ..
+  rm -rf Test28
+}
