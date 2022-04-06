0c3bcc65410d9d95107c09c1423a138fbf95bd8d
Chris Blanton <Chris.Blanton@ldt-4359643.gfdl.noaa.gov>
2022-04-06 15:21:24 -0400

    #99 Use gcc/9.2.0 on workstations, draft. No particular compile flags so far

--- site-configs/gfdl-ws/env.sh
+++ site-configs/gfdl-ws/env.sh
@@ -33,7 +33,6 @@
 
 # Variables to control versions used
 env_version=v0.15
-intel_version=18.0.5
 gcc_version=9.2.0
 ncc_version=4.7.3
 ncf_version=4.5.2
@@ -42,20 +41,18 @@ mpi_version=3.3.2
 # Ensure the base spack modules are first in MODULEPATH
 module remove-path MODULEPATH /app/spack/${env_version}/modulefiles/linux-rhel7-x86_64
 module prepend-path MODULEPATH /app/spack/${env_version}/modulefiles/linux-rhel7-x86_64
-# GCC is needed for icc to use newer C11 constructs
-module load gcc/$gcc_version
 # bats and nccmp are needed for tests
 module load bats/0.4.0
 module load nccmp/1.8.2.0
 
-# Load the Intel compilers
-module load intel_compilers/${intel_version}
+# Load the GCC compiler
+module load gcc/$gcc_version
 
-# Ensure the Intel spack modules are first in MODULEPATH
-module remove-path MODULEPATH /app/spack/${env_version}/modulefiles-intel-${intel_version}/linux-rhel7-x86_64
-module prepend-path MODULEPATH /app/spack/${env_version}/modulefiles-intel-${intel_version}/linux-rhel7-x86_64
+# Ensure the GCC spack modules are first in MODULEPATH
+module remove-path MODULEPATH /app/spack/${env_version}/modulefiles-gcc-${gcc_version}/linux-rhel7-x86_64
+module prepend-path MODULEPATH /app/spack/${env_version}/modulefiles-gcc-${gcc_version}/linux-rhel7-x86_64
 
-# Load the Intel modules required for building
+# Load the GCC modules required for building
 module load netcdf-c/$ncc_version
 module load netcdf-fortran/$ncf_version
 module load mpich/$mpi_version
