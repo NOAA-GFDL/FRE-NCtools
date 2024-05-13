49ace4704d117c764079e416d7a26e62c4d016a8
mlee03 <Mikyung.Lee@lscamd50-d.gfdl.noaa.gov>
2024-05-13 12:54:46 -0400

    add parameters file

---
+++ tools/libfrencutils_acc/parameters.h
@@ -0,0 +1,29 @@
+/***********************************************************************
+ *                   GNU Lesser General Public License
+ *
+ * This file is part of the GFDL FRE NetCDF tools package (FRE-NCTools).
+ *
+ * FRE-NCtools is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * FRE-NCtools is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FRE-NCTools.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ **********************************************************************/
+
+#define AREA_RATIO_THRESH (1.e-6)
+#define MASK_THRESH       (0.5)
+#define MAX_V 8
+
+#define AREA_RATIO (1.e-3)
+#define MAXVAL     (1.e20)
+#define TOLERANCE  (1.e-10)
+
+#define MAXXGRID 1e6
