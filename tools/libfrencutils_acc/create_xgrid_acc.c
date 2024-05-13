d41c4512ff8dbf16b2ef308e73dd59d615df6cac
mlee03 <Mikyung.Lee@lscamd50-d.gfdl.noaa.gov>
2024-05-13 11:39:21 -0400

    create create_xgrid_utils

--- tools/libfrencutils_acc/create_xgrid_acc.c
+++ tools/libfrencutils_acc/create_xgrid_acc.c
@@ -23,80 +23,11 @@
 #include "mosaic_util.h"
 #include "create_xgrid.h"
 #include "create_xgrid_acc.h"
+#include "create_xgrid_utils_acc.h"
 #include "constant.h"
 
 #define AREA_RATIO_THRESH (1.e-6)
 #define MASK_THRESH       (0.5)
-
-/*******************************************************************************
-void get_grid_area(const int *nlon, const int *nlat, const double *lon, const double *lat, const double *area)
-  return the grid area.
-*******************************************************************************/
-void get_grid_area_acc(const int *nlon, const int *nlat, const double *lon, const double *lat, double *area)
-{
-  int nx, ny, nxp, i, j, n_in;
-  double x_in[20], y_in[20];
-
-  nx = *nlon;
-  ny = *nlat;
-  nxp = nx + 1;
-
-  for(j=0; j<ny; j++) for(i=0; i < nx; i++) {
-      x_in[0] = lon[j*nxp+i];
-      x_in[1] = lon[j*nxp+i+1];
-      x_in[2] = lon[(j+1)*nxp+i+1];
-      x_in[3] = lon[(j+1)*nxp+i];
-      y_in[0] = lat[j*nxp+i];
-      y_in[1] = lat[j*nxp+i+1];
-      y_in[2] = lat[(j+1)*nxp+i+1];
-      y_in[3] = lat[(j+1)*nxp+i];
-      n_in = fix_lon(x_in, y_in, 4, M_PI);
-      area[j*nx+i] = poly_area(x_in, y_in, n_in);
-    }
-
-};  /* get_grid_area */
-
-void get_grid_great_circle_area(const int *nlon, const int *nlat, const double *lon, const double *lat, double *area)
-{
-  int nx, ny, nxp, nyp, i, j, n_in;
-  int n0, n1, n2, n3;
-  double x_in[20], y_in[20], z_in[20];
-  struct Node *grid=NULL;
-  double *x=NULL, *y=NULL, *z=NULL;
-
-
-  nx = *nlon;
-  ny = *nlat;
-  nxp = nx + 1;
-  nyp = ny + 1;
-
-  x = (double *)malloc(nxp*nyp*sizeof(double));
-  y = (double *)malloc(nxp*nyp*sizeof(double));
-  z = (double *)malloc(nxp*nyp*sizeof(double));
-
-  latlon2xyz(nxp*nyp, lon, lat, x, y, z);
-
-  for(j=0; j<ny; j++) for(i=0; i < nx; i++) {
-      /* clockwise */
-      n0 = j*nxp+i;
-      n1 = (j+1)*nxp+i;
-      n2 = (j+1)*nxp+i+1;
-      n3 = j*nxp+i+1;
-      rewindList();
-      grid = getNext();
-      addEnd(grid, x[n0], y[n0], z[n0], 0, 0, 0, -1);
-      addEnd(grid, x[n1], y[n1], z[n1], 0, 0, 0, -1);
-      addEnd(grid, x[n2], y[n2], z[n2], 0, 0, 0, -1);
-      addEnd(grid, x[n3], y[n3], z[n3], 0, 0, 0, -1);
-      area[j*nx+i] = gridArea(grid);
-    }
-
-  free(x);
-  free(y);
-  free(z);
-
-};  /* get_grid_great_circle_area */
-
 
 /*******************************************************************************
   void create_xgrid_2DX2D_order1
