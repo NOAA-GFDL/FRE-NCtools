d91885e529b3d3de4cb8e5d150f0689e50bdb3d5
mlee03 <Mikyung.Lee@lscamd50-d.gfdl.noaa.gov>
2024-06-20 10:11:04 -0400

    fix j to jc

--- tools/fregrid/bilinear_interp.c
+++ tools/fregrid/bilinear_interp.c
@@ -225,7 +225,7 @@ void setup_bilinear_interp(int ntiles_in, const Grid_config *grid_in, int ntiles
       for(l=0; l<3*ntiles_in; l++) index[l] = 0;
       for(l=0; l<ntiles_in; l++) shortest[l] = M_PI + M_PI;
       for(l=0; l<ntiles_in; l++) {
-	for(jc=0; jc<ny_in; j++) for(ic=0; ic<nx_in; ic++) {
+	for(jc=0; jc<ny_in; jc++) for(ic=0; ic<nx_in; ic++) {
 	  n1 = jc*nxd+ic;
 	  v1[0] = grid_in[l].xt[n1];
 	  v1[1] = grid_in[l].yt[n1];
