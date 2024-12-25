6108ec0653d70b7caa4bc54782fb73673bc76de8
Seth Underwood <underwoo@underwoo.io>
2024-12-25 14:34:02 -0500

    Migrate test07

--- tools/runoff_regrid/runoff_regrid.c
+++ tools/runoff_regrid/runoff_regrid.c
@@ -18,26 +18,11 @@
  * <http://www.gnu.org/licenses/>.
  **********************************************************************/
 /*
-  This program remaps runoff data from a spherical grid onto any grid
-  (spherical or tripolar) using conservative scheme.
+ This program remaps runoff data from a spherical grid onto any grid
+ (spherical or tripolar) using conservative scheme.
 
- AUTHOR: Zhi Liang (Zhi.Liang@noaa.gov)
-          NOAA Geophysical Fluid Dynamics Lab, Princeton, NJ
-
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  For the full text of the GNU General Public License,
-  write to: Free Software Foundation, Inc.,
-            675 Mass Ave, Cambridge, MA 02139, USA.
------------------------------------------------------------------------
+ AUTHOR: Zhi Liang
+ NOAA Geophysical Fluid Dynamics Lab, Princeton, NJ
 */
 
 #include <stdlib.h>
@@ -52,7 +37,7 @@
 #include "create_xgrid.h"
 #include "tool_util.h"
 
-#define  EPSLN10 (1.e-10)
+#define EPSLN10 (1.e-10)
 #define D2R (M_PI/180)
 #define MAXATT   4096
 
