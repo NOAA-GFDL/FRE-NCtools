13248328127cf632ae8937018addc66e94833ee5
Ian Laflotte <ilaflott@gmail.com>
2025-09-19 16:58:43 -0400

    friday YOLO: try the build directory option.

--- build.sh
+++ build.sh
@@ -4,24 +4,25 @@ pwd
 echo "Contents of SRC_DIR / Build directory are:"
 ls
 
-## this is sufficient
-autoreconf -i
-./configure --prefix=$PREFIX || cat config.log
+### this is sufficient
+#autoreconf -i
+#./configure --prefix=$PREFIX || cat config.log
 #./configure --prefix=$PREFIX --with-mpi || cat config.log
 #./configure --prefix=$PREFIX --enable-quad-precision || cat config.log
 #./configure --prefix=$PREFIX --enable-quad-precision --with-mpi || cat config.log
-echo "compiling/building"
-make
-echo "installing into $PREFIX"
-make install
-
-### to test, build-dir option, ala README
-#autoreconf -i
-#mkdir build && cd build
-#../configure --prefix=$PREFIX || cat config.log
-##../configure --prefix=$PREFIX --with-mpi || cat config.log
-##../configure --prefix=$PREFIX --enable-quad-precision || cat config.log
-##../configure --prefix=$PREFIX --enable-quad-precision --with-mpi || cat config.logecho "compiling/building"
+#echo "compiling/building"
 #make
 #echo "installing into $PREFIX"
 #make install
+
+## to test, build-dir option, ala README
+autoreconf -i
+mkdir build && cd build
+../configure --prefix=$PREFIX || cat config.log
+#../configure --prefix=$PREFIX --with-mpi || cat config.log
+#../configure --prefix=$PREFIX --enable-quad-precision || cat config.log
+#../configure --prefix=$PREFIX --enable-quad-precision --with-mpi || cat config.logecho "compiling/building"
+echo "compiling/building at $PWD"
+make
+echo "installing into $PREFIX"
+make install
