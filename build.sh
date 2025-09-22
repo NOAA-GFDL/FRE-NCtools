2a86e696a60ed921975bdedb58fcba0e6bdea29f
Ian Laflotte <ilaflott@gmail.com>
2025-09-22 13:12:52 -0400

    try pinning the gcc compiler. try setting the LD_LIBRARY_PATH to PREFIX/lib, check if we have gcc in the testing step and guard against throwing an error if we dont find it

--- build.sh
+++ build.sh
@@ -4,12 +4,33 @@ pwd
 echo "Contents of SRC_DIR / Build directory are:"
 ls
 
+export LD_LIBRARY_PATH=${PREFIX}/lib
+echo "PRE CONFIGURATION::"
+echo ""
+echo ""
+echo "PATH is:"
+echo $PATH
+echo "LD_LIBRARY_PATH is:"
+echo $LD_LIBRARY_PATH
+echo ""
+
 ## this is sufficient
 autoreconf -i
 ./configure --prefix=$PREFIX || cat config.log
 #./configure --prefix=$PREFIX --with-mpi || cat config.log
 #./configure --prefix=$PREFIX --enable-quad-precision || cat config.log
 #./configure --prefix=$PREFIX --enable-quad-precision --with-mpi || cat config.log
+
+export LD_LIBRARY_PATH=${PREFIX}/lib
+echo "PRE CONFIGURATION::"
+echo ""
+echo ""
+echo "PATH is:"
+echo $PATH
+echo "LD_LIBRARY_PATH is:"
+echo $LD_LIBRARY_PATH
+echo ""
+
 echo "compiling/building"
 make
 echo "installing into $PREFIX"
