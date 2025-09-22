fea46cc1a21d778986b1c5e728e0c33f99e8b0e8
Ian Laflotte <ilaflott@gmail.com>
2025-09-22 11:19:34 -0400

    Revert "friday YOLO: try the build directory option."
    This reverts commit 13248328127cf632ae8937018addc66e94833ee5.

--- build.sh
+++ build.sh
@@ -4,25 +4,24 @@ pwd
 echo "Contents of SRC_DIR / Build directory are:"
 ls
 
-### this is sufficient
-#autoreconf -i
-#./configure --prefix=$PREFIX || cat config.log
+## this is sufficient
+autoreconf -i
+./configure --prefix=$PREFIX || cat config.log
 #./configure --prefix=$PREFIX --with-mpi || cat config.log
 #./configure --prefix=$PREFIX --enable-quad-precision || cat config.log
 #./configure --prefix=$PREFIX --enable-quad-precision --with-mpi || cat config.log
-#echo "compiling/building"
-#make
-#echo "installing into $PREFIX"
-#make install
-
-## to test, build-dir option, ala README
-autoreconf -i
-mkdir build && cd build
-../configure --prefix=$PREFIX || cat config.log
-#../configure --prefix=$PREFIX --with-mpi || cat config.log
-#../configure --prefix=$PREFIX --enable-quad-precision || cat config.log
-#../configure --prefix=$PREFIX --enable-quad-precision --with-mpi || cat config.logecho "compiling/building"
-echo "compiling/building at $PWD"
+echo "compiling/building"
 make
 echo "installing into $PREFIX"
 make install
+
+### to test, build-dir option, ala README
+#autoreconf -i
+#mkdir build && cd build
+#../configure --prefix=$PREFIX || cat config.log
+##../configure --prefix=$PREFIX --with-mpi || cat config.log
+##../configure --prefix=$PREFIX --enable-quad-precision || cat config.log
+##../configure --prefix=$PREFIX --enable-quad-precision --with-mpi || cat config.logecho "compiling/building"
+#make
+#echo "installing into $PREFIX"
+#make install
