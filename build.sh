30ab51c90decaca42b276d0ddec4781168e3a52e
Ian Laflotte <ilaflott@gmail.com>
2025-09-25 13:37:46 -0400

    lets try configuring with nf-config and nc-config

--- build.sh
+++ build.sh
@@ -2,72 +2,92 @@
 
 set -ex
 
+echo ""
 which nc-config
 echo "we have nc-config"
+echo ""
+
+echo ""
 which nf-config
 echo "we have nf-config"
+echo ""
 
-#CC=
-#FC=
-#CFLAGS=
-#FCFLAGS=
-#LDFLAGS=#
+#echo ""
+#PATH=
+#echo "PATH is:"
+#echo $PATH
+#echo ""
+#RPATH=
+#echo "RPATH is:"
+#echo $RPATH
+#echo ""
 #LD_LIBRARY_PATH=${PREFIX}/lib
+#echo "LD_LIBRARY_PATH is:"
+#echo $LD_LIBRARY_PATH
+#echo ""
+#DYLD_LIBRARY_PATH=
+#echo "DYLD_LIBRARY_PATH is:"
+#echo $DYLD_LIBRARY_PATH
+#echo ""
 
-echo "PRE CONFIGURATION::"
 echo ""
-echo "PATH is:"
-echo $PATH
-echo "RPATH is:"
-echo $RPATH
-echo "LD_LIBRARY_PATH is:"
-echo $LD_LIBRARY_PATH
-echo "DYLD_LIBRARY_PATH is:"
-echo $DYLD_LIBRARY_PATH
-echo ""
-echo ""
+#CC=mpicc
+CC=`nc-config --cc`
 echo "CC is:"
 echo $CC
-echo "CF is:"
-echo $CF
+echo ""
+
+echo ""
+#FC=mpifc
+FC=`nf-config --fc`
+echo "FC is:"
+echo $FC
+echo ""
+
+echo ""
+CFLAGS=`nc-config --cflags`
 echo "CFLAGS is:"
 echo $CFLAGS
+echo ""
+
+echo ""
+FCFLAGS=`nf-config --fflags`
 echo "FCFLAGS is:"
 echo $FCFLAGS
+echo ""
+
+echo ""
+LDFLAGS=`nc-config --libs`
 echo "LDFLAGS is:"
 echo $LDFLAGS
+echo ""
 
-
-echo 'building FRE-NCtools conda package...'
+echo ""
 echo "SRC_DIR / Build directory is:"
 pwd
 echo "Contents of SRC_DIR / Build directory are:"
 ls
+echo ""
+
+echo ""
+echo 'building FRE-NCtools conda package...'
 
 ## this is sufficient
 autoreconf -iv
-./configure --prefix=$PREFIX || cat config.log
+./configure --with-mpi || cat config.log
 #./configure --prefix=$PREFIX --with-mpi || cat config.log
-#./configure --prefix=$PREFIX --enable-quad-precision || cat config.log
+#./configure --prefix=$PREFIX --with-mpi || cat config.log
 #./configure --prefix=$PREFIX --enable-quad-precision --with-mpi || cat config.log
 
+echo ""
 echo "compiling/building"
 make
 
-echo "installing into $PREFIX"
+echo ""
+#echo "installing into $PREFIX"
+echo "installing no PREFIX"
 make install
 
-
-
-
-####################################################
-### to test, build-dir option, ala README
-#autoreconf -iv
-#mkdir build && cd build
-#../configure --prefix=$PREFIX || cat config.log
-##../configure --prefix=$PREFIX --with-mpi || cat config.log
-##../configure --prefix=$PREFIX --enable-quad-precision || cat config.log
-##../configure --prefix=$PREFIX --enable-quad-precision --with-mpi || cat config.logecho "compiling/building"
-#make
-#echo "installing into $PREFIX"
-#make install
+echo ""
+echo "trying a make check in the build process, not advisable but i want info"
+make check RUN_EXPENSIVE_TESTS=no
