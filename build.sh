4ae2956b8c28098e016ac3f942c99d1260835309
Ian Laflotte <ilaflott@gmail.com>
2025-09-25 12:53:41 -0400

    exploring guidance from Mikyung, and Frank

--- build.sh
+++ build.sh
@@ -1,27 +1,53 @@
-echo 'builindg FRE-NCtools conda package...'
+#!/bin/bash
+
+set -ex
+
+which nc-config
+which nf-config
+
+echo 'building FRE-NCtools conda package...'
 echo "SRC_DIR / Build directory is:"
 pwd
 echo "Contents of SRC_DIR / Build directory are:"
 ls
 
-export LD_LIBRARY_PATH=${PREFIX}/lib
 echo "PRE CONFIGURATION::"
-echo ""
 echo ""
 echo "PATH is:"
 echo $PATH
+echo "RPATH is:"
+echo $RPATH
 echo "LD_LIBRARY_PATH is:"
 echo $LD_LIBRARY_PATH
+echo "DYLD_LIBRARY_PATH is:"
+echo $DYLD_LIBRARY_PATH
 echo ""
+echo ""
+echo "CC is:"
+echo $CC
+echo "CF is:"
+echo $CF
+echo "CFLAGS is:"
+echo $CFLAGS
+echo "FCFLAGS is:"
+echo $FCFLAGS
+echo "LDFLAGS is:"
+echo $LDFLAGS
+
+#CC=
+#FC=
+#CFLAGS=
+#FCFLAGS=
+#LDFLAGS=#
+#LD_LIBRARY_PATH=${PREFIX}/lib
 
 ## this is sufficient
-autoreconf -i
+autoreconf -iv
 ./configure --prefix=$PREFIX || cat config.log
 #./configure --prefix=$PREFIX --with-mpi || cat config.log
 #./configure --prefix=$PREFIX --enable-quad-precision || cat config.log
 #./configure --prefix=$PREFIX --enable-quad-precision --with-mpi || cat config.log
 
-export LD_LIBRARY_PATH=${PREFIX}/lib
 echo "POST CONFIGURATION::"
 echo ""
 echo ""
@@ -33,11 +59,12 @@ echo ""
 
 echo "compiling/building"
 make
+
 echo "installing into $PREFIX"
 make install
 
 ### to test, build-dir option, ala README
-#autoreconf -i
+#autoreconf -iv
 #mkdir build && cd build
 #../configure --prefix=$PREFIX || cat config.log
 ##../configure --prefix=$PREFIX --with-mpi || cat config.log
