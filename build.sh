bb130942839290835a0371b622b8d69b7136e32f
Ian Laflotte <ilaflott@gmail.com>
2025-09-25 14:05:18 -0400

    try uploading test-suite log first before the tarball for now, because the build process now makes the tests and runs them and moves the test-suite log. break up a long line, comment-out the testing section of meta.yaml for now

--- build.sh
+++ build.sh
@@ -90,4 +90,8 @@ make install
 
 echo ""
 echo "trying a make check in the build process, not advisable but i want info"
-make check RUN_EXPENSIVE_TESTS=no
+make check RUN_EXPENSIVE_TESTS=no \
+	|| echo "make check failed- see test-suite.log, guarding against the failure to not clobber helpful output"
+
+cp tests/test-suite.log /app/fre-nctools/tarball/test-suite.log || echo "copying test-suite log failed"
+
